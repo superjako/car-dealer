110dc7eef695a6016395883ac884198a3f102fb3
2890 <sunpeng@hydee>
2020-09-25 14:14:33 +0800

    文件上传修改

--- car-dealer-api/src/main/java/com/sg/api/FileUploadController.java
+++ car-dealer-api/src/main/java/com/sg/api/FileUploadController.java
@@ -55,40 +55,7 @@ public class FileUploadController {
         }
         return result;
     }
-   /* @RequestMapping(value = "/uploadFile", method = RequestMethod.POST)
-    public R uploadPicture(@ApiParam("图片") MultipartFile file, HttpServletRequest request, HttpServletResponse response) {
-        File targetFile = null;
-        String url = "";//返回存储路径
-        String fileName = file.getOriginalFilename();//获取文件名加后缀
-        if (fileName != null && fileName != "") {
-            String returnUrl = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort() + request.getContextPath() + "/upload/imgs/";//存储路径
-            String path = request.getSession().getServletContext().getRealPath("upload/imgs"); //文件存储位置
-            String fileF = fileName.substring(fileName.lastIndexOf("."), fileName.length());//文件后缀
-            fileName = System.currentTimeMillis() + "_" + new Random().nextInt(1000) + fileF;//新的文件名
 
-            //先判断文件是否存在
-            SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
-            String fileAdd = sdf.format(new Date());
-            //获取文件夹路径
-            File file1 = new File(path + "/" + fileAdd);
-            //如果文件夹不存在则创建
-            if (!file1.exists() && !file1.isDirectory()) {
-                file1.mkdir();
-            }
-            //将图片存入文件夹
-            targetFile = new File(file1, fileName);
-            try {
-                //将上传的文件写到服务器上指定的文件。
-                file.transferTo(targetFile);
-                url = returnUrl + fileAdd + "/" + fileName;
-                return R.ok(url);
-            } catch (Exception e) {
-                e.printStackTrace();
-                return R.failed("系统异常，图片上传失败");
-            }
-        }
-        return R.ok(null);
-    }*/
 
     private StringBuffer getFileSubstr(HttpServletRequest request) {
         StringBuffer sdf = new StringBuffer();
